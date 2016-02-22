e4d4272f3eef3bde552ae42e04e2fda6f8a2db0f
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-02-22 01:12:09 +0100

    init

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                MOUHAHA
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,17 @@
 import gulp from 'gulp'
 import browserSync from 'browser-sync'
 import nodemon from 'gulp-nodemon'
+import jsx from 'gulp-jsx'
 
-gulp.task('serve', () => {
+gulp.task('build', () => {
+    return gulp.src('./app/*.jsx')
+        .pipe(jsx({
+            factory: 'React.createClass'
+        }))
+        .pipe(gulp.dest('dist'))
+})
+
+gulp.task('serve', ['build'] , () => {
     browserSync.init(null, {
         proxy: 'http://localhost:7777',
         port: 9001
