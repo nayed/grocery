1e46ddb03c0e8a67bd75d7f6d039293197920894
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-02-27 22:37:19 +0100

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
 var path = require('path')
-var config = {
-  entry: path.resolve(__dirname, 'app/main.jsx'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-    }]
-  }
+var node_modules = path.resolve(__dirname, 'node_modules')
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js')
+
+config = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.jsx')],
+    resolve: {
+        alias: {
+          'react': pathToReact
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel'
+        }, {
+            test: /\.css$/, // Only .css files
+            loader: 'style!css' // Run both loaders
+        }],
+        noParse: [pathToReact]
+    }
 }
 
 module.exports = config
