5666d24f703876ca02004463c8616475d992b2e2
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-02-27 23:12:14 +0100

    if item is purchased let's cross it

--- app/components/GroceryItem.jsx
+++ app/components/GroceryItem.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export class GroceryItem extends React.Component {
     render() {
         return (
-            <div>{this.props.item.name}</div>
+            <div>
+                <h4 className={this.props.item.purchased ? "strikethrough" : ""}>{this.props.item.name}</h4>
+            </div>
         )
     }
 }
